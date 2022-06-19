4af49a9e326ff6114792391df3d7e9b8bf73509e
Foystor <foystor@gmail.com>
2022-06-19 10:06:41 +0800

    feat: Create Model Classes - the Reservation class

---
+++ src/model/Reservation.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class Reservation {
+
+    private final Customer customer;
+    private final IRoom room;
+    private final Date checkInDate;
+    private final Date checkOutDate;
+
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public IRoom getRoom() {
+        return room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        String checkInDateDetail = formatter.format(checkInDate);
+        String checkOutDateDetail = formatter.format(checkOutDate);
+        return customer.toString() + "\n" + room.toString() + "\nCheckin Date: " + checkInDateDetail + "\nCheckout Date: " + checkOutDateDetail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reservation that = (Reservation) o;
+        return Objects.equals(customer, that.customer) && Objects.equals(room, that.room) && Objects.equals(checkInDate, that.checkInDate) && Objects.equals(checkOutDate, that.checkOutDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customer, room, checkInDate, checkOutDate);
+    }
+}
