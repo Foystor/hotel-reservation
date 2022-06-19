752b91a05e706a7eb9d9850091cb9ba0f5ce292a
Foystor <foystor@gmail.com>
2022-06-19 12:01:59 +0800

    feat: Functionalize the AdminMenu item - Add a Room

--- src/menu/AdminMenu.java
+++ src/menu/AdminMenu.java
@@ -2,10 +2,11 @@ package menu;
 
 import api.AdminResource;
 import api.HotelResource;
-import model.Customer;
-import model.IRoom;
+import model.*;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminMenu {
@@ -52,7 +53,7 @@ public class AdminMenu {
                     case 4 -> {
                         System.out.println("Add a Room");
                         keepRunning = false;
-                        //addARoom();
+                        addARoom();
                     }
                     case 5 -> {
                         System.out.println("Back to Main Menu");
@@ -112,5 +113,94 @@ public class AdminMenu {
         adminResource.displayAllReservations();
         // back to the admin menu
         printAdminMenu();
+    }
+
+
+    /**
+     * add a room
+     */
+    public void addARoom() {
+        boolean keepRunning = true;
+        List<IRoom> rooms = new LinkedList<>();
+
+        while (keepRunning) {
+            try {
+                // add a room to the list
+                rooms.add(createARoom(rooms));
+                System.out.println("Would you like to add another room? y/n");
+                String selection = scanner.nextLine();
+
+                if (selection.equalsIgnoreCase("y")) {
+                    continue;
+                } else if (selection.equalsIgnoreCase("n")) {
+                    // add the rooms
+                    adminResource.addRoom(rooms);
+                    keepRunning = false;
+                    // back to the admin menu
+                    printAdminMenu();
+                } else {
+                    throw new IllegalArgumentException("Please enter y or n");
+                }
+            } catch (Exception ex) {
+                System.out.println("Invalid Input: " + ex.getMessage());
+            }
+        }
+    }
+
+
+    /**
+     * create a room
+     * @param rooms the room list created by this turn
+     * @return IRoom object
+     */
+    private IRoom createARoom(Collection<IRoom> rooms) {
+        boolean keepRunning = true;
+        String roomNumber = null;
+        Double roomPrice = null;
+        RoomType roomType = null;
+
+        while (keepRunning) {
+            try {
+                System.out.println("Enter room number");
+                try {
+                    String input = scanner.nextLine();
+                    Integer.parseInt(input);
+                    roomNumber = input;
+                } catch (Exception ex) {
+                    throw new IllegalArgumentException("Please enter a number");
+                }
+
+                // check if the room already exists
+                for (IRoom room : rooms) {
+                    if (room.getRoomNumber().equals(roomNumber)) throw new IllegalArgumentException("Room already exists!");
+                }
+                if (hotelResource.getRoom(roomNumber) != null) throw new IllegalArgumentException("Room already exists!");
+
+                System.out.println("Enter price per night");
+                try {
+                    roomPrice = Double.parseDouble(scanner.nextLine());
+                } catch (Exception ex) {
+                    throw new IllegalArgumentException("Please enter a price");
+                }
+
+                System.out.println("Enter room type: 1 for single bed, 2 for double bed");
+                int roomTypeNum = Integer.parseInt(scanner.nextLine());
+                if (roomTypeNum == 1) {
+                    roomType = RoomType.SINGLE;
+                } else if (roomTypeNum == 2) {
+                    roomType = RoomType.DOUBLE;
+                } else {
+                    throw new IllegalArgumentException("Please enter 1 or 2");
+                }
+
+                keepRunning = false;
+            } catch (Exception ex) {
+                System.out.println("Invalid Input: " + ex.getMessage());
+            }
+        }
+
+        if (roomPrice == 0.0) return new FreeRoom(roomNumber, roomType);
+
+        return new Room(roomNumber, roomPrice, roomType);
     }
 }
